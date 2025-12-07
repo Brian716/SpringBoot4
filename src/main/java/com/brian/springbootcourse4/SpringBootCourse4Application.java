fe5608e203ac2a5a273d2883ab997ef91dbbcbfd
“Brian <brian.edu25@gmail.com>
2025-12-06 19:19:58 -0500

    Using application.properties

---
+++ src/main/java/com/brian/springbootcourse4/SpringBootCourse4Application.java
@@ -0,0 +1,13 @@
+package com.brian.springbootcourse4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourse4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCourse4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/brian/springbootcourse4/SpringBootCourse4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.springbootcourse4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourse4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
